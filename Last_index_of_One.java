ed3beb1aec75a183f12d03d1c4284089519bb93b
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-11-25 11:12:59 +0530

    Create Last_index_of_One.java
    Given a string S consisting only '0's and '1's,  find the last index of the '1' present in it.
    
     
    
    Example 1:
    
    Input:
    S = 00001
    Output:
    4
    Explanation:
    Last index of  1 in given string is 4.
     
    
    Example 2:
    
    Input:
    0
    Output:
    -1
    Explanation:
    Since, 1 is not present, so output is -1.

---
+++ Last_index_of_One.java
@@ -0,0 +1,12 @@
+// Last index of One
+
+class Solution {
+    public int lastIndex( String s) {
+        for(int i=s.length()-1; i>=0; i--){
+	        if(s.charAt(i)=='1'){
+	            return i;
+	        }
+        }
+        return -1;
+    }
+}

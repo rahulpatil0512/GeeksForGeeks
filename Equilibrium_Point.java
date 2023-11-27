f5c425e7ab37545fd9ad26ec976c7a444eb5e146
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-11-27 23:21:05 +0530

    Create Equilibrium_Point.java
    Given an array A of n positive numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is an index (or position) such that the sum of all elements before that index is same as sum of elements after it.
    
    Note: Return equilibrium point in 1-based indexing. Return -1 if no such point exists. 
    
    Example 1:
    
    Input: 
    n = 5 
    A[] = {1,3,5,2,2} 
    Output: 
    3 
    Explanation:  
    equilibrium point is at position 3 as sum of elements before it (1+3) = sum of elements after it (2+2). 
    Example 2:
    
    Input:
    n = 1
    A[] = {1}
    Output: 
    1
    Explanation:
    Since there's only element hence its only the equilibrium point.

---
+++ Equilibrium_Point.java
@@ -0,0 +1,25 @@
+// Equilibrium Point
+
+class Solution {
+
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        int sr=0;
+        int sl=0;
+        if(n==1){
+            return 1;
+        }
+        for(int i=1; i<n; i++){
+            sr+=arr[i];
+        }
+        for(int i=0; i<n-1; i++){
+            if(sl==sr){
+                return i+1;
+            }
+            sl+=arr[i];
+            sr-=arr[i+1];
+        }
+        return -1;
+    }
+}

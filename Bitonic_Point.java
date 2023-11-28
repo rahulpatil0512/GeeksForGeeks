393f6db93664d40d245140992c7f8abfc953ed1e
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-11-28 19:46:46 +0530

    Create Bitonic_Point.java
    Given an array arr of n elements that is first strictly increasing and then maybe strictly decreasing, find the maximum element in the array.
    Note: If the array is increasing then just print the last element will be the maximum value.
    
    Example 1:
    
    Input: 
    n = 9
    arr[] = {1,15,25,45,42,21,17,12,11}
    Output: 45
    Explanation: Maximum element is 45.
    Example 2:
    
    Input: 
    n = 5
    arr[] = {1, 45, 47, 50, 5}
    Output: 50
    Explanation: Maximum element is 50.

---
+++ Bitonic_Point.java
@@ -0,0 +1,20 @@
+// Bitonic Point
+
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        // code here
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return arr[start];
+    }
+}

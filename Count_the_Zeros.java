5191f6d18f4b97385a144c578497e0d8e1139ea8
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-11-28 19:51:37 +0530

    Create Count_the_Zeros.java
    Given an array of size N consisting of only 0's and 1's. The array is sorted in such a manner that all the 1's are placed first and then they are followed by all the 0's. Find the count of all the 0's.
    
    Example 1:
    
    Input:
    N = 12
    Arr[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}
    Output: 3
    Explanation: There are 3 0's in the given array.
    
    Example 2:
    
    Input:
    N = 5
    Arr[] = {0, 0, 0, 0, 0}
    Output: 5
    Explanation: There are 5 0's in the array.

---
+++ Count_the_Zeros.java
@@ -0,0 +1,14 @@
+// Count the Zeros
+
+class Solution {
+    int countZeroes(int[] arr, int n) {
+        // code here
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                count+=1;
+            }
+        }
+        return count;
+    }
+}

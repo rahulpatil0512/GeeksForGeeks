a848fef214926ac9afea3fb802c49e21742a68d8
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-12-16 19:26:33 +0530

    Create Find_Missing_And_Repeating.java
    Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2,....,N} is missing and one number 'B' occurs twice in array. Find these two numbers.
    
    Example 1:
    
    Input:
    N = 2
    Arr[] = {2, 2}
    Output: 2 1
    Explanation: Repeating number is 2 and 
    smallest positive missing number is 1.
    Example 2:
    
    Input:
    N = 3
    Arr[] = {1, 3, 3}
    Output: 3 2
    Explanation: Repeating number is 3 and 
    smallest positive missing number is 2.

---
+++ Find_Missing_And_Repeating.java
@@ -0,0 +1,22 @@
+// Find Missing And Repeating
+
+class Solve {
+     int[] findTwoElement(int arr[], int n) {
+        int[] ans = new int[2];
+        for (int i = 0; i < n; i++) {
+            int abs_val = Math.abs(arr[i]);
+            if (arr[abs_val - 1] > 0){
+                arr[abs_val - 1] = -arr[abs_val - 1];
+            }
+            else{
+                ans[0] = abs_val;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0){
+                ans[1] = i + 1;
+            }
+        }
+        return ans;
+    }
+}

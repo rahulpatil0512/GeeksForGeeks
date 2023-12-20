fbfc5e31cb6b7c9ed7db8ca938cd59138606f9be
Rahul Patil <86288483+rahulpatil0512@users.noreply.github.com>
2023-12-20 18:41:46 +0530

    Create Prime_Number.java
    For a given number N check if it is prime or not. A prime number is a number which is only divisible by 1 and itself.
     
    
    Example 1:
    
    Input:
    N = 5
    Output:
    1
    Explanation:
    5 has 2 factors 1 and 5 only.
    Example 2:
    
    Input:
    N = 25
    Output:
    0
    Explanation:
    25 has 3 factors 1, 5, 25

---
+++ Prime_Number.java
@@ -0,0 +1,13 @@
+// Prime Number
+
+class Solution{
+    static int isPrime(int N) 
+    { 
+       if(N == 1)  
+            return 0;
+        for(int i=2;i*i<=N;i++)  
+            if(N%i==0)  
+                return 0;
+        return 1;  
+    } 
+}
